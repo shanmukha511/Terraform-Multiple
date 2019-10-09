09552296e18e2a46aa41c58d81bab6085131039c
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-09 01:44:28 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,5 +8,5 @@ variable "client_secret" {}
 variable "os_disk" {
   description = "source_uri of VM_OS Disk"
   type        = list(string)
-  default     = ["/subscriptions/a8a59231-c575-45fb-8e16-2b4c5b6a2271/resourceGroups/pwc-test/providers/Microsoft.Compute/disks/WindowsVM1_OsDisk_1_4d828544cc0d4cd5a7885ee1f556fbc3", "/subscriptions/a8a59231-c575-45fb-8e16-2b4c5b6a2271/resourceGroups/pwc-test/providers/Microsoft.Compute/disks/UbuntuVM2_disk1_f8c640745009457e8bc98578e5757af9"]
+  default     = ["/subscriptions/a8a59231-c575-45fb-8e16-2b4c5b6a2271/resourceGroups/PWC-TEST/providers/Microsoft.Compute/disks/WindowsVM_OsDisk_1_180acba49f3b44b0ba61e36fd78e4d18", "/subscriptions/a8a59231-c575-45fb-8e16-2b4c5b6a2271/resourceGroups/pwc-test/providers/Microsoft.Compute/disks/UbuntuVM_OsDisk_1_f08a771634bc4a00812bc581bbbed97d"]
 }
